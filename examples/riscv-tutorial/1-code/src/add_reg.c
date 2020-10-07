51d78f59167aec8568208aabfd0abdd56dabdcdf
Didrik Lundberg <didrikl@kth.se>
2020-10-07 13:14:45 +0200

    Fixed bug in add_reg.c

--- examples/riscv-tutorial/1-code/src/add_reg.c
+++ examples/riscv-tutorial/1-code/src/add_reg.c
@@ -3,10 +3,10 @@
 #ifdef BAREMETAL
 
 int64_t add_reg (int64_t x, int64_t y) {
-  register int64_t lx asm ("x2") = x;
-  register int64_t ly asm ("x3") = y;
-  register int64_t oldx asm ("x4") = x;
-  register int64_t oldy asm ("x5") = y;
+  register int64_t lx asm ("a0") = x;
+  register int64_t ly asm ("a1") = y;
+  register int64_t oldx asm ("a2") = x;
+  register int64_t oldy asm ("a3") = y;
   while (lx > 0) {
     ly += 1;
     lx -= 1;
