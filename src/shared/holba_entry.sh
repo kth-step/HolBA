71b5ca7351736a4a2ed5a8a2c2707a3074ad183b
Andreas Lindner <andili@kth.se>
2020-10-22 01:02:34 +0200

    Fix automatic building and heap path

--- src/shared/holba_entry.sh
+++ src/shared/holba_entry.sh
@@ -5,6 +5,8 @@ set -e
 SHARED_DIR=$(dirname "${BASH_SOURCE[0]}")
 HOLBA_DIR=$(readlink -f "${SHARED_DIR}/../..")
 
+CURRENT_DIR=$(pwd)
+
 if [[ "$1" == "QUICK" ]]; then
   # we need at least 2 arguments
   if [[ "$#" -lt 2 ]]; then
@@ -28,14 +30,15 @@ else
 fi
 
 if [[ "${QUICK_RUN}" == "NO" ]]; then
-  make -C "${HOLBA_DIR}" "src"
+  MAKETARGET=$(python3 -c "import os.path; print(os.path.relpath('${CURRENT_DIR}', '${HOLBA_DIR}'))")
+  make -C "${HOLBA_DIR}" "${MAKETARGET}"
 fi
 
 # source the overall environment script
 set --
 source "${HOLBA_DIR}/env.sh"
 
-HEAPNAME=../HolBA-heap
+HEAPNAME=${HOLBA_DIR}/src/HolBA-heap
 BUILDHEAP=${HOLBA_HOL_DIR}/bin/buildheap
 
 "${BUILDHEAP}" --gcthreads=1 --holstate="${HEAPNAME}" "${SCRIPT_NAME}" --extra="${FORWARD_ARGS}"
