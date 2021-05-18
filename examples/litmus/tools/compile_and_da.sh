1a02c1a2b46b3e3e60b0c560ad80f51a77d283a8
Henrik Karlsson <henrik10@kth.se>
2021-05-18 18:02:21 +0200

    Fixed flag for litmus assembly compiler

--- examples/litmus/tools/compile_and_da.sh
+++ examples/litmus/tools/compile_and_da.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-
 DIR=$(dirname "${BASH_SOURCE[0]}")
 source $DIR/../../../config.env.sh
-AS=${HOLBA_GCC_RISCV64_CROSS}as
-OBJDUMP=${HOLBA_GCC_RISCV64_CROSS}objdump
+
+CROSS=${HOLBA_GCC_RISCV64_CROSS}
+AS=${CROSS}as
+OBJDUMP=${CROSS}objdump
+
+AS_FLAGS=-march=rv64ima
 
 TMP_S=$(mktemp /tmp/XXXXXX.s)
 TMP_BIN=${TMP_S}.bin
 TMP_DA=${TMP_S}.da
+
 cat - > $TMP_S && \
-	$AS $TMP_S -o $TMP_BIN && \
+	$AS $AS_FLAGS $TMP_S -o $TMP_BIN && \
 	$OBJDUMP -d $TMP_BIN > $TMP_DA && \
 	printf $TMP_DA
