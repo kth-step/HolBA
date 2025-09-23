5528f7a9b8e7a31c1ede7b2045673649c3ebb059
Andreas Lindner <andili@kth.se>
2025-09-23 10:33:59 +0200

    Add log pulling

--- src_exectime/holba-se_run_all.sh
+++ src_exectime/holba-se_run_all.sh
@@ -11,11 +11,26 @@ HOLBA_DIR=$(readlink -f "${HOLBA_DIR}")
 
 cd $HOLBA_DIR
 
+# run all verifications
 make examples/arm_cm0/tiny
 make examples/arm_cm0/aes
 make examples/arm_cm0/modexp_simple
 make examples/arm_cm0/motor_set
 make examples/arm_cm0/balrob/v2-faster
 
-# TODO: write all relevant logs to: logs/armcm0/holba-se/
-# TODO: remove unimportant parts of logs from the files manually
+# pull all relevant logs
+mkdir -p logs/armcm0/holba-se
+
+cp examples/arm_cm0/tiny/.hollogs/tiny_analysisTheory logs/armcm0/holba-se/ldldbr8
+
+cp examples/arm_cm0/aes/.hollogs/aes_o0_wholeTheory logs/armcm0/holba-se/aes
+
+cp examples/arm_cm0/modexp_simple/.hollogs/modexp_asmTheory logs/armcm0/holba-se/modexp
+cp examples/arm_cm0/modexp_simple/.hollogs/modexp_asm_uidivmodTheory logs/armcm0/holba-se/modexp_uidivmod
+
+cp examples/arm_cm0/motor_set/.hollogs/motor_o0Theory logs/armcm0/holba-se/motor_set
+cp examples/arm_cm0/motor_set/.hollogs/motor_o3Theory logs/armcm0/holba-se/motor_set_o3
+
+cp examples/arm_cm0/balrob/v2-faster/.hollogs/balrob_topTheory logs/armcm0/holba-se/balrob
+cp examples/arm_cm0/balrob/v2-faster/.hollogs/balrob_faddTheory logs/armcm0/holba-se/balrob_fadd
+cp examples/arm_cm0/balrob/v2-faster/.hollogs/balrob_fdivTheory logs/armcm0/holba-se/balrob_fdiv
